0b507ca6ff32f1f8425ced10bd27e5f0eb1c3ab8
Nuri Amari <nuri.amari99@gmail.com>
2020-03-14 13:45:51 -0700

    Differentiate between token resolution errors and tokenization errors

--- lexer/exceptions.py
+++ lexer/exceptions.py
@@ -1,4 +1,19 @@
-class LexicalError(Exception):
+from typing import Optional
 
+from lexer.utils import LexerStreamReader
+
+
+class LexicalError(Exception):
+    def __init__(self, reader: LexerStreamReader, message: Optional[str] = None):
+        error_line, error_col, error_line_content = reader.get_curr_line()
+        error_header: str = f"LexicalError: Line {error_line}, column {error_col}"
+        error_pointer: str = " " * (error_col - 1) + "^"
+        error_message: str = message or ""
+        super().__init__(
+            f"{error_header}\n{error_line_content}\n{error_pointer}\n{error_message}"
+        )
+
+
+class TokenResolutionError(Exception):
     def __init__(self, message):
-        super().__init__(f'LexicalError: {message}')
+        super().__init__(f"TokenResolutionError: {message}")
