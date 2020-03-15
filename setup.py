4007e6e52094285783ed7b0db519c518c95e2611
Nuri Amari <nuri@athinkingape.com>
2020-03-14 17:21:00 -0700

    Make package pip installable

---
+++ setup.py
@@ -0,0 +1,22 @@
+import setuptools
+
+with open("README.md", "r") as read_me:
+    long_description = read_me.read()
+
+setuptools.setup(
+    name="langtools-NuriAmari",
+    version="0.0.1",
+    author="Nuri Amari",
+    author_email="nuri.amari99@gmail.com",
+    description="A small example package",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    url="https://github.com/NuriAmari/Language-Tools",
+    packages=setuptools.find_packages(),
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ],
+    python_requires='>=3.8',
+)
