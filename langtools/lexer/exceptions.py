e109635db36236fd6ba53df1b5022a7523194ad7
Nuri Amari <nuri.amari99@gmail.com>
2020-03-22 11:25:52 -0700

    Better lexical error with useful fields

--- langtools/lexer/exceptions.py
+++ langtools/lexer/exceptions.py
@@ -4,11 +4,17 @@ from typing import Optional
 class LexicalError(Exception):
     def __init__(self, reader, message: Optional[str] = None):
         error_line, error_col, error_line_content = reader.get_curr_line()
-        error_header: str = f"LexicalError: Line {error_line}, column {error_col}"
-        error_pointer: str = " " * (error_col - 1) + "^"
-        error_message: str = message or ""
+        error_header: str = f"LexicalError: Line {error_line}, Column {error_col}"
+        error_pointer: str = " " * (error_col) + "^"
+        error_char = error_line_content[error_col]
+        error_message: str = message or f'Unexpected Character: "{error_char}"'
+
+        self.error_line = error_line
+        self.error_col = error_col
+        self.error_char = error_char
+
         super().__init__(
-            f"{error_header}\n{error_line_content}\n{error_pointer}\n{error_message}"
+            f"\n{error_header}\n{error_line_content}\n{error_pointer}\n{error_message}"
         )
 
 
